50ebde6905cc6b8ce5f9fe8cc09c3bdf2b5b3bf7
Posa <Posa@DESKTOP-G0GJ4AC>
2023-05-22 11:45:41 +0200

    Dodana forma za evaluaciju

--- WindowsFormsApp1/WindowsFormsApp1/FrmEvaluation.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/FrmEvaluation.Designer.cs
@@ -28,190 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.txtMinForSignature = new System.Windows.Forms.TextBox();
+            this.txtMinForGrade = new System.Windows.Forms.TextBox();
+            this.txtActivityDescription = new System.Windows.Forms.TextBox();
+            this.txtEvaluationDate = new System.Windows.Forms.TextBox();
+            this.txtTeacher = new System.Windows.Forms.TextBox();
+            this.numPoints = new System.Windows.Forms.NumericUpDown();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.cboActivities = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numPoints)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtMinForSignature
             // 
-            this.textBox1.Location = new System.Drawing.Point(78, 49);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
+            this.txtMinForSignature.Location = new System.Drawing.Point(333, 54);
+            this.txtMinForSignature.Name = "txtMinForSignature";
+            this.txtMinForSignature.Size = new System.Drawing.Size(95, 22);
+            this.txtMinForSignature.TabIndex = 1;
             // 
-            // textBox2
+            // txtMinForGrade
             // 
-            this.textBox2.Location = new System.Drawing.Point(538, 49);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(88, 22);
-            this.textBox2.TabIndex = 1;
+            this.txtMinForGrade.Location = new System.Drawing.Point(476, 54);
+            this.txtMinForGrade.Name = "txtMinForGrade";
+            this.txtMinForGrade.Size = new System.Drawing.Size(100, 22);
+            this.txtMinForGrade.TabIndex = 3;
             // 
-            // textBox4
+            // txtActivityDescription
             // 
-            this.textBox4.Location = new System.Drawing.Point(652, 49);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 3;
+            this.txtActivityDescription.Location = new System.Drawing.Point(27, 107);
+            this.txtActivityDescription.Multiline = true;
+            this.txtActivityDescription.Name = "txtActivityDescription";
+            this.txtActivityDescription.Size = new System.Drawing.Size(549, 162);
+            this.txtActivityDescription.TabIndex = 4;
             // 
-            // textBox3
+            // txtEvaluationDate
             // 
-            this.textBox3.Location = new System.Drawing.Point(109, 128);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(579, 22);
-            this.textBox3.TabIndex = 4;
+            this.txtEvaluationDate.Location = new System.Drawing.Point(301, 326);
+            this.txtEvaluationDate.Name = "txtEvaluationDate";
+            this.txtEvaluationDate.Size = new System.Drawing.Size(100, 22);
+            this.txtEvaluationDate.TabIndex = 5;
             // 
-            // textBox5
+            // txtTeacher
             // 
-            this.textBox5.Location = new System.Drawing.Point(508, 337);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 22);
-            this.textBox5.TabIndex = 5;
+            this.txtTeacher.Location = new System.Drawing.Point(166, 325);
+            this.txtTeacher.Name = "txtTeacher";
+            this.txtTeacher.Size = new System.Drawing.Size(100, 22);
+            this.txtTeacher.TabIndex = 6;
             // 
-            // textBox6
+            // numPoints
             // 
-            this.textBox6.Location = new System.Drawing.Point(665, 324);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(100, 22);
-            this.textBox6.TabIndex = 6;
+            this.numPoints.Location = new System.Drawing.Point(456, 326);
+            this.numPoints.Name = "numPoints";
+            this.numPoints.Size = new System.Drawing.Size(120, 22);
+            this.numPoints.TabIndex = 7;
             // 
-            // numericUpDown1
+            // btnSave
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(297, 303);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 22);
-            this.numericUpDown1.TabIndex = 7;
+            this.btnSave.Location = new System.Drawing.Point(385, 382);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "Spremi";
+            this.btnSave.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnCancel
             // 
-            this.button1.Location = new System.Drawing.Point(554, 399);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(691, 404);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnCancel.Location = new System.Drawing.Point(501, 382);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Odustani";
+            this.btnCancel.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(94, 25);
+            this.label1.Location = new System.Drawing.Point(24, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(64, 16);
             this.label1.TabIndex = 10;
-            this.label1.Text = "label1";
+            this.label1.Text = "Aktivnost:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(549, 23);
+            this.label2.Location = new System.Drawing.Point(330, 24);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.Size = new System.Drawing.Size(98, 16);
             this.label2.TabIndex = 11;
-            this.label2.Text = "label2";
+            this.label2.Text = "Uvjet za potpis:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(672, 26);
+            this.label3.Location = new System.Drawing.Point(473, 23);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.Size = new System.Drawing.Size(101, 16);
             this.label3.TabIndex = 12;
-            this.label3.Text = "label3";
+            this.label3.Text = "Uvjet za ocjenu:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(332, 279);
+            this.label4.Location = new System.Drawing.Point(163, 292);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.Size = new System.Drawing.Size(73, 16);
             this.label4.TabIndex = 13;
-            this.label4.Text = "label4";
+            this.label4.Text = "Ocjenjivač:";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(517, 301);
+            this.label5.Location = new System.Drawing.Point(298, 292);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.Size = new System.Drawing.Size(114, 16);
             this.label5.TabIndex = 14;
-            this.label5.Text = "label5";
+            this.label5.Text = "Datum evaluacije:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(694, 297);
+            this.label6.Location = new System.Drawing.Point(453, 292);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(44, 16);
+            this.label6.Size = new System.Drawing.Size(53, 16);
             this.label6.TabIndex = 15;
-            this.label6.Text = "label6";
+            this.label6.Text = "Bodovi:";
+            // 
+            // cboActivities
+            // 
+            this.cboActivities.FormattingEnabled = true;
+            this.cboActivities.Location = new System.Drawing.Point(27, 54);
+            this.cboActivities.Name = "cboActivities";
+            this.cboActivities.Size = new System.Drawing.Size(238, 24);
+            this.cboActivities.TabIndex = 16;
             // 
             // FrmEvaluation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(616, 427);
+            this.Controls.Add(this.cboActivities);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.numPoints);
+            this.Controls.Add(this.txtTeacher);
+            this.Controls.Add(this.txtEvaluationDate);
+            this.Controls.Add(this.txtActivityDescription);
+            this.Controls.Add(this.txtMinForGrade);
+            this.Controls.Add(this.txtMinForSignature);
             this.Name = "FrmEvaluation";
             this.Text = "FrmEvaluation";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPoints)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMinForSignature;
+        private System.Windows.Forms.TextBox txtMinForGrade;
+        private System.Windows.Forms.TextBox txtActivityDescription;
+        private System.Windows.Forms.TextBox txtEvaluationDate;
+        private System.Windows.Forms.TextBox txtTeacher;
+        private System.Windows.Forms.NumericUpDown numPoints;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cboActivities;
     }
 }
